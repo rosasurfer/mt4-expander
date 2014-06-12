15ee5ef84bfe7807f1c01df079996ac80f3759c8
Peter Walther <pewa-bitbucket@rosasurfer.com>
2014-06-12 07:05:23 +0000

    *** empty log message ***

--- src/Expander.cpp
+++ src/Expander.cpp
@@ -1,2 +1,38 @@
-
 #include "stdafx.h"
+
+
+/**
+ *
+ */
+int GetInteger(int value) { 
+   return(value);
+
+   #pragma EXPORT
+}
+
+
+/**
+ *
+ */
+double GetDouble(double value) {
+   return(value);
+
+   #pragma EXPORT
+}
+
+
+/**
+ * Gibt den übergebenen Zeiger auf einen String zurück. Kann in MQL zum Lesen eines Strings von einer Adresse 
+ * verwendet werden.
+ *
+ * @param  char* str - Zeiger
+ *
+ * @return char* - derselbe Zeiger
+ *
+ * NOTE: MQL-Importdeklaration:  string GetString(int address);
+ */
+char* GetString(char* str) {
+   return(str);
+
+   #pragma EXPORT
+}
