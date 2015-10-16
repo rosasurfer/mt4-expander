ee4e02c9d0cb9bb6bf534ac1f8bc5781f67be0cf
Peter Walther <pewa-bitbucket@rosasurfer.com>
2015-10-16 13:58:53 +0000

    struct POSITION_DATA hinzugefügt

--- src/mql/structs/myfx/CustomPosition.cpp
+++ src/mql/structs/myfx/CustomPosition.cpp
@@ -1,5 +1,5 @@
 /**
- * Header von Historydateien (Kursreihen im "history"-Verzeichnis).
+ *
  */
 #include "stdafx.h"
 #include "global.h"
@@ -7,20 +7,47 @@
 
 
 /**
- * Binäre Representation eines einzelnen Konfigurationsterms einer individuellen Position. Eine Position kann aus ein
+ * Binäre Repräsentation eines einzelnen Konfigurationsterms einer individuellen Position. Eine Position kann aus ein
  * oder mehreren mit Kommas voneinander getrennten Termen bestehen.
  *
- * In custom_position_conf[] markiert ein nicht initialisiertes Struct (pct.type=NULL) das Ende einer Position.
+ * In positions_config[] markiert ein nicht initialisiertes Struct (pct.type=NULL) das Ende einer Position.
  *
- * @see  MQL: double custom.position.conf[][5];
+ * @see  MQL: double positions.config[][5];
  */
 struct POSITION_CONFIG_TERM {
-   double type;            // [ Ticket | TYPE_OPEN_LONG | TYPE_OPEN_SHORT | TYPE_OPEN_SYMBOL | TYPE_OPEN_ALL | TYPE_HISTORY_SYMBOL | TYPE_HISTORY_ALL | TYPE_REALIZED | TYPE_EQUITY ]
-   double value1;          // [ EMPTY  | LotSize | FromDateTime ]
-   double value2;          // [ Price  | Amount  | ToDateTime   ]
-   double value3;          // Speicher für Zwischenergebnisse bei Berechnung der Kennziffern einer Position
-   double value4;          // Speicher für Zwischenergebnisse bei Berechnung der Kennziffern einer Position
-} pct;
+   double type;               // [ Ticket | TERM_OPEN_LONG | TERM_OPEN_SHORT | TERM_OPEN_SYMBOL | TERM_OPEN_ALL | TERM_HISTORY_SYMBOL | TERM_HISTORY_ALL | TERM_ADJUSTMENT | TERM_EQUITY ]
+   double confValue1;         // [ EMPTY  | LotSize | FromDateTime ]
+   double confValue2;         // [ Price  | Amount  | ToDateTime   ]
+   double cacheValue1;        // Speicher für Zwischenergebnisse bei Berechnung der Kennziffern von TERM_HISTORY_*
+   double cacheValue2;        // Speicher für Zwischenergebnisse bei Berechnung der Kennziffern von TERM_HISTORY_*
+};
+
+POSITION_CONFIG_TERM positions_config[];
 
 
-POSITION_CONFIG_TERM custom_position_conf[];
+/**
+ * Report-Datensatz einer Position zur Anzeige im Chart.
+ *
+ * @see  MQL: int    positions.idata[][3];
+ *       MQL: double positions.ddata[][9];
+ */
+struct POSITION_DATA {
+   uint   configType;         // [ CONFIG_AUTO | CONFIG_REAL | CONFIG_VIRTUAL ]
+   uint   positionType;       // [ POSITION_LONG | POSITION_SHORT | POSITION_HEDGE | POSITION_HISTORY ]
+   uint   commentIndex;       // Index des Positionskommentars in positions.config.comments[]
+
+   double directionalLots;    // ungehedgte Lots
+   double hedgedLots;         // gehedgte Lots
+ union {
+   double breakevenPrice;     // BreakevenPrice, wenn (directionalLots != 0)
+   double pipDistance;        // Pip-Distance,   wenn (hedgedLots && !directionalLots)
+ };
+   double openEquity;         // Equity bei PositionOpen (Maßstab für fullProfitPct)
+   double openProfit;         // P/L offener Trades
+   double closedProfit;       // P/L geschlossener Trades
+   double adjustedProfit;     // P/L-Adjustment
+   double fullProfitAbs;      // gesamter P/L absolut
+   double fullProfitPct;      // gesamter P/L prozentual
+};
+
+POSITION_DATA positions_data[];
