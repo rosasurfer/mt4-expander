e45346e1e83d849f88ae3ed065f69ffbb2164016
Peter Walther <pewa-bitbucket@rosasurfer.com>
2016-03-07 22:12:21 +0000

    *** empty log message ***

--- header/Expander.h
+++ header/Expander.h
@@ -239,10 +239,10 @@ struct FXT_HEADER {                                // -- offset ---- size --- de
    char   serverName[128];                         //        68       128     Name des Accountservers (szchar)
    char   symbol[MAX_SYMBOL_LENGTH+1];             //       196        12     Symbol (szchar)
    uint   period;                                  //       208         4     Timeframe in Minuten
-   uint   modelType;                               //       212         4     0=every tick
-   uint   modeledBars;                             //       216         4     Anzahl der modellierten Bars (nach Prolog)
-   uint   firstBarTime;                            //       220         4     OpenTime der Bar des ersten Ticks (nach Prolog)
-   uint   lastBarTime;                             //       224         4     OpenTime der Bar des letzten Ticks (nach Prolog)
+   uint   modelType;                               //       212         4     0=EveryTick|1=ControlPoints|2=BarOpen
+   uint   modeledBars;                             //       216         4     Anzahl der modellierten Bars (ohne Prolog)
+   uint   firstBarTime;                            //       220         4     OpenTime der Bar des ersten Ticks (ohne Prolog)
+   uint   lastBarTime;                             //       224         4     OpenTime der Bar des letzten Ticks (ohne Prolog)
    BYTE   reserved_1[4];                           //       228         4     (alignment to the next double)
    double modelQuality;                            //       232         8     max. 99.9
 
@@ -256,18 +256,18 @@ struct FXT_HEADER {                                // -- offset ---- size --- de
    uint   maxLotsize;                              //       276         4     Höchst-Lotsize in Hundertsteln Lot         = MarketInfo(MODE_MAXLOT)  * 100
    uint   lotStepsize;                             //       280         4     Lot-Stepsize in Hundertsteln Lot           = MarketInfo(MODE_LOTSTEP) * 100
    uint   stopDistance;                            //       284         4     Stop level in Points                       = MarketInfo(MODE_STOPLEVEL)
-   BOOL   pendingsGTC;                             //       288         4     ob PendingOrders good-till-cancel sind       @see struct SYMBOL
+   BOOL   pendingsGTC;                             //       288         4     close pending orders at end of day or GTC    @see struct SYMBOL
    BYTE   reserved_3[4];                           //       292         4     (alignment to the next double)
 
    // profit calculation parameters                // ----------------------------------------------------------------------------------------------------------------
    double contractSize;                            //       296         8     Größe eines Lots, z.B. 100000              = MarketInfo(MODE_LOTSIZE)
    double tickValue;                               //       304         8     Tick-Value in Quote-Currency ???           = MarketInfo(MODE_TICKVALUE)
    double tickSize;                                //       312         8     Tick-Größe, z.B. 0.0000'1                  = MarketInfo(MODE_TICKSIZE)
-   uint   profitCalculationMode;                   //       320         4     profit calculation mode                    = MarketInfo(MODE_PROFITCALCMODE)
+   uint   profitCalculationMode;                   //       320         4     0=Forex|1=CFD|2=Futures                    = MarketInfo(MODE_PROFITCALCMODE)
 
    // swap calculation parameters                  // ----------------------------------------------------------------------------------------------------------------
    BOOL   swapEnabled;                             //       324         4     ob Swaps berechnet werden
-   uint   swapCalculationMode;                     //       328         4     swap calculation mode                      = MarketInfo(MODE_SWAPTYPE)
+   uint   swapCalculationMode;                     //       328         4     0=Points|1=BaseCurrency|2=Interest|3=MarginCurrency   = MarketInfo(MODE_SWAPTYPE)
    BYTE   reserved_4[4];                           //       332         4     (alignment to the next double)
    double swapLongValue;                           //       336         8     long overnight swap value                  = MarketInfo(MODE_SWAPLONG)
    double swapShortValue;                          //       344         8     short overnight swap values                = MarketInfo(MODE_SWAPSHORT)
@@ -292,16 +292,20 @@ struct FXT_HEADER {                                // -- offset ---- size --- de
    uint   commissionType;                          //       436         4     0=RoundTurn|1=PerDeal                        @see COMMISSION_TYPE_*
 
    // later additions                              // ----------------------------------------------------------------------------------------------------------------
-   uint   firstBar;                                //       440         4     bar number/index??? of first bar (nach Prolog) or 0 for first bar
-   uint   lastBar;                                 //       444         4     bar number/index??? of last bar (nach Prolog) or 0 for last bar
-   uint   startPeriod[6];                          //       448        24     [0] = firstBar
-   uint   inputFrom;                               //       472         4     start date as specified by the user
-   uint   inputTo;                                 //       476         4     end date as specified by the user
+   uint   firstBar;                                //       440         4     bar number/index??? of first bar (ohne Prolog) or 0 for first bar
+   uint   lastBar;                                 //       444         4     bar number/index??? of last bar (ohne Prolog) or 0 for last bar
+   uint   startPeriod[6];                          //       448        24     [0] = firstBar (bar where the smaller period modeling started)
+   uint   testerSettingFrom;                       //       472         4     begin date from tester settings
+   uint   testerSettingTo;                         //       476         4     end date from tester settings
    uint   freezeDistance;                          //       480         4     freeze level in points                     = MarketInfo(MODE_FREEZELEVEL)
    uint   modelErrors;                             //       484         4     number of errors during model generation
    BYTE   reserved_6[240];                         //       488       240     unused
 };                                                 // ----------------------------------------------------------------------------------------------------------------
                                                    //               = 728     warum bin ich nicht auf Ibiza
+
+
+// Tickdata, letztes Feld: // expert flag 0-bar is modified, but expert is not run
+
 
 /**
  * Wrapper für eine nach MQL zu übermittelnde Logmessage.
