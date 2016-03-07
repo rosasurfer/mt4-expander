7593121f692a9bb02226dd117a8c43951b4b996d
Peter Walther <pewa-bitbucket@rosasurfer.com>
2016-03-07 19:15:02 +0000

    *** empty log message ***

--- header/Expander.h
+++ header/Expander.h
@@ -138,28 +138,28 @@ struct SYMBOL_SELECTED {                           // -- offset ---- size --- de
    char   symbol[MAX_SYMBOL_LENGTH+1];             //         0        12     Symbol (szchar)
    uint   digits;                                  //        12         4     Digits
 
-   uint   symbolIndex;                             //        16         4     Index des Symbols in "symbols.raw"
-   DWORD  undocumented_1;                          //        20         4     immer 0x0001
+   uint   index;         // oder ID ?????          //        16         4     Index/ID des Symbols in "symbols.raw"
+   DWORD  unknown_1;                               //        20         4     immer 0x0001
 
-   uint   groupIndex;                              //        24         4     Index der Symbolgruppe in "symgroups.raw"
-   DWORD  undocumented_2;                          //        28         4
+   uint   group;                                   //        24         4     Index der Symbolgruppe in "symgroups.raw"
+   DWORD  unknown_2;                               //        28         4
 
-   double point;                                   //        32         8     Point
-   uint   fixedSpread;                             //        40         4     fester Spread in Points oder NULL bei variablem Spread
-   DWORD  undocumented_3;                          //        44         4
+   double pointSize;                               //        32         8     Point size
+   uint   spread;                                  //        40         4     Spread: 0=current spread (variable)
+   DWORD  unknown_3;                               //        44         4
 
    uint   tickType;                                //        48         4     Tick-Type: 0=Uptick, 1=Downtick, 2=n/a
-   WORD   undocumented_4;                          //        52         2     immer 0x0100
-   WORD   undocumented_5;                          //        54         2     unterschiedlich (oft NULL)
+   WORD   unknown_4;                               //        52         2     immer 0x0100
+   WORD   unknown_5;                               //        54         2     unterschiedlich (oft NULL)
 
    uint   time;                                    //        56         4     Time
-   DWORD  undocumented_6;                          //        60         4
+   DWORD  unknown_6;                               //        60         4
    double bid;                                     //        64         8     Bid
    double ask;                                     //        72         8     Ask
    double sessionHigh;                             //        80         8     Session High
    double sessionLow;                              //        88         8     Session Low
 
-   BYTE   undocumented_7[16];                      //        96        16     unterschiedlich (oft alles NULL)
+   BYTE   unknown_7[16];                           //        96        16     unterschiedlich (oft alles NULL)
    double bid_2;                                   //       112         8     Bid (Wiederholung)
    double ask_2;                                   //       120         8     Ask (Wiederholung)
 };                                                 // ----------------------------------------------------------------------------------------------------------------
