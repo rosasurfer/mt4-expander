69b49f41e7f2d5074e99f7866c612e7644a88411
Peter Walther <pewa-bitbucket@rosasurfer.com>
2016-03-15 22:44:28 +0000

    *** empty log message ***

--- header/Expander.h
+++ header/Expander.h
@@ -157,6 +157,19 @@ struct SYMBOL_SELECTED {                           // -- offset ---- size --- de
                                                    //               = 128
 
 /**
+ * MT4 structure TICK (Dateiformat "ticks.raw")
+ */
+struct TICK {                                      // -- offset ---- size --- description ----------------------------------------------------------------------------
+   char   symbol[MAX_SYMBOL_LENGTH+1];             //         0        12     Symbol (szchar)
+   uint   time;                                    //        12         4     Timestamp
+   double bid;                                     //        16         8     Bid
+   double ask;                                     //        24         8     Ask
+   uint   counter;                                 //        32         4     fortlaufender Zähler innerhalb der Datei
+   BYTE   unknown[4];                              //        36         4     ?
+};                                                 // ----------------------------------------------------------------------------------------------------------------
+                                                   //                = 40
+
+/**
  * MT4 structure HISTORY_HEADER
  *
  * HistoryFile Header
