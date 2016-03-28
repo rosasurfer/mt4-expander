2e9e848db0eabb00ef54ef963aa07eb7a6e3289f
Peter Walther <pewa-bitbucket@rosasurfer.com>
2016-03-28 14:42:29 +0000

    *** empty log message ***

--- header/Expander.h
+++ header/Expander.h
@@ -196,9 +196,9 @@ struct HISTORY_HEADER {                            // -- offset ---- size --- de
    char  symbol[MAX_SYMBOL_LENGTH+1];              //        68        12     Symbol       (szchar)
    uint  period;                                   //        80         4     Timeframe in Minuten
    uint  digits;                                   //        84         4     Digits
-   uint  syncMark;                                 //        88         4     SyncMarker (timestamp), wird vom Terminal beim Refresh mit Serverversion überschrieben
-   uint  lastSync;                                 //        92         4     LastSync   (unbenutzt), wird vom Terminal nicht überschrieben
-   uint  timezoneId;              // custom (pewa) //        96         4     Timezone-ID (default: 0 => Server-Timezone)
+   uint  syncMarker;                               //        88         4     SyncMarker   (timestamp), wird vom Terminal beim Refresh mit Serverversion überschrieben
+   uint  lastSyncTime;                             //        92         4     LastSyncTime (unbenutzt), wird vom Terminal nicht überschrieben
+   uint  timezoneId;              // custom (pewa) //        96         4     Timezone-ID  (default: 0 => Server-Timezone)
    BYTE  reserved[48];                             //       100        48
 };                                                 // ----------------------------------------------------------------------------------------------------------------
                                                    //               = 148
