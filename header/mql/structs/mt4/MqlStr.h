3916e61f76dffb0bf7d862dcd5cbcc02285c43f6
Peter Walther <pewa-bitbucket@rosasurfer.com>
2016-07-09 09:18:38 +0000

    *** empty log message ***

--- header/mql/structs/mt4/MqlStr.h
+++ header/mql/structs/mt4/MqlStr.h
@@ -9,3 +9,6 @@ struct MqlStr {
    int   sizeAlloc;                                // Größe des alloziierten Speicherblocks (0, wenn es ein intern verwaltetes C-Literal ist)
    char* string;
 };
+
+
+// @see  neues Format: https://docs.mql4.com/basis/types/stringconst
