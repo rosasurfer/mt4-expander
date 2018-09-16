fbab8e144323f27b861d45dfae11ab9067d7739f
Peter Walther <pewa-github@rosasurfer.com>
2018-09-16 15:25:34 +0300

    implement file function IsSymlink()

--- src/util/file.cpp
+++ src/util/file.cpp
@@ -12,8 +12,8 @@ BOOL WINAPI IsDirectory(const char* name) {
    if (name) {
       if ((uint)name < MIN_VALID_POINTER) return(error(ERR_INVALID_PARAMETER, "invalid parameter name = 0x%p (not a valid pointer)", name));
 
-      DWORD dwAttrib = GetFileAttributes(name);
-      return(dwAttrib!=INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+      DWORD attrib = GetFileAttributes(name);
+      return(attrib!=INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_DIRECTORY));
    }
    return(FALSE);
    #pragma EXPANDER_EXPORT
@@ -31,9 +31,53 @@ BOOL WINAPI IsFile(const char* name) {
    if (name) {
       if ((uint)name < MIN_VALID_POINTER) return(error(ERR_INVALID_PARAMETER, "invalid parameter name = 0x%p (not a valid pointer)", name));
 
-      DWORD dwAttrib = GetFileAttributes(name);
-      return(dwAttrib!=INVALID_FILE_ATTRIBUTES && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+      DWORD attrib = GetFileAttributes(name);
+      return(attrib!=INVALID_FILE_ATTRIBUTES && !(attrib & FILE_ATTRIBUTE_DIRECTORY));
    }
    return(FALSE);
+   #pragma EXPANDER_EXPORT
+}
+
+
+/**
+ * Whether or not the specified file or directory exists and is a symbolic link.
+ *
+ * @param  char* name - full filename with support for forward and backward slashes
+ *
+ * @return BOOL
+ */
+BOOL WINAPI IsSymlink(const char* name) {
+   BOOL result = FALSE;
+
+   if (name) {
+      if ((uint)name < MIN_VALID_POINTER) return(error(ERR_INVALID_PARAMETER, "invalid parameter name = 0x%p (not a valid pointer)", name));
+
+      DWORD attrib = GetFileAttributes(name);
+
+      if (attrib!=INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_REPARSE_POINT)) {
+         char* _name = strcpy(new char[strlen(name)+1], name);                // on the heap
+
+         int pos = -1;
+         while (_name[++pos]) {                                               // convert forward slashes
+            if (_name[pos] == '/')
+               _name[pos] = '\\';
+         }
+         if (attrib & FILE_ATTRIBUTE_DIRECTORY) {
+            while (--pos >=0 && _name[pos]=='\\') {                           // trim trailing backslashes
+               _name[pos] = '\0';
+            }                                                                 // TODO: completely canonicalize path
+         }
+
+         WIN32_FIND_DATA wfd = {};
+         HANDLE hFind = FindFirstFile(_name, &wfd);
+         if (hFind == INVALID_HANDLE_VALUE) error(ERR_WIN32_ERROR+GetLastError(), "cannot find file \"%s\"", _name);
+         else {
+            FindClose(hFind);
+            result = (wfd.dwReserved0 == IO_REPARSE_TAG_SYMLINK);
+         }
+         delete[] _name;
+      }
+   }
+   return(result);
    #pragma EXPANDER_EXPORT
 }
