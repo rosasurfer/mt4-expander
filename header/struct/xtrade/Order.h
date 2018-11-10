599b504294486352f6308a3e45156d204c10bf2c
Peter Walther <pewa-github@rosasurfer.com>
2018-11-10 21:40:26 +0200

    fix mis-alignment of struct ORDER

--- header/struct/xtrade/Order.h
+++ header/struct/xtrade/Order.h
@@ -13,21 +13,25 @@ struct ORDER {                                        // -- offset --- size --- 
    uint     id;                                       //         0        4     unique order id (positive, primary key)
    int      ticket;                                   //         4        4
    int      type;                                     //         8        4
-   double   lots;                                     //        12        8     (mis-aligned)
-   char     symbol[MAX_SYMBOL_LENGTH+1];              //        20       12
-   double   openPrice;                                //        32        8
-   datetime openTime;                                 //        40        4
-   double   stopLoss;                                 //        44        8     (mis-aligned)
-   double   takeProfit;                               //        52        8     (mis-aligned)
-   double   closePrice;                               //        60        8     (mis-aligned)
-   datetime closeTime;                                //        68        4
-   double   commission;                               //        72        8
-   double   swap;                                     //        80        8
-   double   profit;                                   //        88        8
-   int      magicNumber;                              //        96        4
-   char     comment[MAX_ORDER_COMMENT_LENGTH+1];      //       100       28
+   BYTE     _alignment1;                              //        12        4     (alignment to the next double)
+   double   lots;                                     //        16        8
+   char     symbol[MAX_SYMBOL_LENGTH+1];              //        24       12
+   BYTE     _alignment2;                              //        36        4     (alignment to the next double)
+   double   openPrice;                                //        40        8
+   datetime openTime;                                 //        48        4
+   BYTE     _alignment3;                              //        52        4     (alignment to the next double)
+   double   stopLoss;                                 //        56        8
+   double   takeProfit;                               //        64        8
+   double   closePrice;                               //        72        8
+   datetime closeTime;                                //        80        4
+   BYTE     _alignment4;                              //        84        4     (alignment to the next double)
+   double   commission;                               //        88        8
+   double   swap;                                     //        96        8
+   double   profit;                                   //       104        8
+   int      magicNumber;                              //       112        4
+   char     comment[MAX_ORDER_COMMENT_LENGTH+1];      //       116       28
 };                                                    // ----------------------------------------------------------------------------
-#pragma pack(pop)                                     //              = 128
+#pragma pack(pop)                                     //              = 144
 
 
 typedef std::vector<ORDER> OrderHistory;
