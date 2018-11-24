89901493f66b6c3d9b0048bd15071d08a2a5ab7f
Peter Walther <pewa-github@rosasurfer.com>
2018-11-24 17:10:49 +0200

    documentation

--- src/lib/conversion.cpp
+++ src/lib/conversion.cpp
@@ -574,6 +574,18 @@ const char* WINAPI ModuleTypeToStr(ModuleType type) {
 
 /**
  * Alias of numberFormat()
+ *
+ * Format a numeric value as a std::string.
+ *
+ * @param  doube value
+ * @param  char* format - format control string as used for printf()
+ *
+ * @return string - formatted string or an empty string in case of errors
+ *
+ *       Format codes:
+ * @see  https://alvinalexander.com/programming/printf-format-cheat-sheet
+ * @see  http://www.cplusplus.com/reference/cstdio/printf/
+ * @see  ms-help://MS.VSCC.v90/MS.MSDNQTR.v90.en/dv_vccrt/html/664b1717-2760-4c61-bd9c-22eee618d825.htm
  */
 string WINAPI numberToStr(double value, const char* format) {
    return(numberFormat(value, format));
@@ -582,6 +594,18 @@ string WINAPI numberToStr(double value, const char* format) {
 
 /**
  * Alias of NumberFormat()
+ *
+ * Format a numeric value as a C string.
+ *
+ * @param  doube value
+ * @param  char* format - format control string as used for printf()
+ *
+ * @return char* - formatted string or a NULL pointer in case of errors
+ *
+ *       Format codes:
+ * @see  https://alvinalexander.com/programming/printf-format-cheat-sheet
+ * @see  http://www.cplusplus.com/reference/cstdio/printf/
+ * @see  ms-help://MS.VSCC.v90/MS.MSDNQTR.v90.en/dv_vccrt/html/664b1717-2760-4c61-bd9c-22eee618d825.htm
  */
 const char* WINAPI NumberToStr(double value, const char* format) {
    return(NumberFormat(value, format));
