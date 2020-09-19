19e2994e5902dca68e681ffd923e5dd3160bbff6
Peter Walther <pewa-github@rosasurfer.com>
2020-09-19 07:17:49 +0300

    rename LogMessageA() to AppendLogMessageA()

--- src/lib/log.cpp
+++ src/lib/log.cpp
@@ -18,7 +18,7 @@ extern MqlProgramList g_mqlPrograms;               // all MQL programs: vector<C
  *
  * @return BOOL - success status
  */
-BOOL WINAPI LogMessageA(EXECUTION_CONTEXT* ec, const char* message, int error, int level) {
+BOOL WINAPI AppendLogMessageA(EXECUTION_CONTEXT* ec, const char* message, int error, int level) {
    if ((uint)ec < MIN_VALID_POINTER)      return(error(ERR_INVALID_PARAMETER, "invalid parameter ec: 0x%p (not a valid pointer)", ec));
    if (!ec->pid)                          return(error(ERR_INVALID_PARAMETER, "invalid execution context: ec.pid=0  ec=%s", EXECUTION_CONTEXT_toStr(ec)));
    if (g_mqlPrograms.size() <= ec->pid)   return(error(ERR_ILLEGAL_STATE,     "invalid execution context: ec.pid=%d (no such program)  ec=%s", ec->pid, EXECUTION_CONTEXT_toStr(ec)));
