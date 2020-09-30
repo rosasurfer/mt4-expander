a006170f970d2bbc01cab71c00f0daf74bc3b69f
Peter Walther <pewa-github@rosasurfer.com>
2020-09-30 06:22:10 +0300

    add field EXECUTION_CONTEXT.logBuffer

--- header/shared/bak/defines.h
+++ header/shared/bak/defines.h
@@ -318,7 +318,8 @@
 #define INIT_TIMEZONE                           1        // initialize/check the timezone configuration
 #define INIT_PIPVALUE                           2        // check availability of the current pip value (requires tick size and value)
 #define INIT_BARS_ON_HIST_UPDATE                4        //
-#define INIT_NO_BARS_REQUIRED                   8        // executable without history (scripts only)
+#define INIT_NO_BARS_REQUIRED                   8        // executable without chart history (scripts only)
+#define INIT_BUFFERED_LOG                      16        // setup a logfile buffer for logging
 
 
 // MT4 internal messages
