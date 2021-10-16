4854e9ff9d2d32d92d5b0c9243a96b2f832f0e60
Peter Walther <pewa-github@rosasurfer.com>
2021-10-16 19:37:12 +0300

    drop constant INIT_AUTOCONFIG

--- header/shared/bak/defines.h
+++ header/shared/bak/defines.h
@@ -328,8 +328,7 @@
 #define INIT_BARS_ON_HIST_UPDATE                4        //
 #define INIT_NO_BARS_REQUIRED                   8        // executable without chart history (scripts only)
 #define INIT_BUFFERED_LOG                      16        // setup a logfile buffer for logging
-#define INIT_AUTOCONFIG                        32        // setup auto-configuration
-#define INIT_NO_EXTERNAL_REPORTING             64        // always disable external test reporting (experts only)
+#define INIT_NO_EXTERNAL_REPORTING             32        // always disable external test reporting (experts only)
 
 
 // MT4 internal messages
