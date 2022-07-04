f9bced93e5d51844ed3aec38cc2b8ce4f0ada685
Peter Walther <pewa-github@rosasurfer.com>
2022-07-04 22:57:23 +0300

    add more custom timeframes

--- src/lib/conversion.cpp
+++ src/lib/conversion.cpp
@@ -1348,19 +1348,27 @@ const char* WINAPI PeriodDescriptionA(int period) {
    switch (period) {
       case NULL      : return("NULL");
       case PERIOD_M1 : return("M1");      // 1 minute
+      case PERIOD_M2 : return("M2");      // 2 minutes  (custom timeframe)
+      case PERIOD_M3 : return("M3");      // 3 minutes  (custom timeframe)
+      case PERIOD_M4 : return("M4");      // 4 minutes  (custom timeframe)
       case PERIOD_M5 : return("M5");      // 5 minutes
+      case PERIOD_M6 : return("M6");      // 6 minutes  (custom timeframe)
+      case PERIOD_M10: return("M10");     // 10 minutes (custom timeframe)
+      case PERIOD_M12: return("M12");     // 12 minutes (custom timeframe)
       case PERIOD_M15: return("M15");     // 15 minutes
+      case PERIOD_M20: return("M20");     // 20 minutes (custom timeframe)
       case PERIOD_M30: return("M30");     // 30 minutes
       case PERIOD_H1 : return("H1");      // 1 hour
-      case PERIOD_H2 : return("H2");      // 2 hours (custom timeframe)
-      case PERIOD_H3 : return("H3");      // 3 hours (custom timeframe)
+      case PERIOD_H2 : return("H2");      // 2 hours    (custom timeframe)
+      case PERIOD_H3 : return("H3");      // 3 hours    (custom timeframe)
       case PERIOD_H4 : return("H4");      // 4 hours
-      case PERIOD_H6 : return("H6");      // 6 hours (custom timeframe)
-      case PERIOD_H8 : return("H8");      // 8 hours (custom timeframe)
+      case PERIOD_H6 : return("H6");      // 6 hours    (custom timeframe)
+      case PERIOD_H8 : return("H8");      // 8 hours    (custom timeframe)
+      case PERIOD_H12: return("H12");     // 12 hours   (custom timeframe)
       case PERIOD_D1 : return("D1");      // 1 day
       case PERIOD_W1 : return("W1");      // 1 week
       case PERIOD_MN1: return("MN1");     // 1 month
-      case PERIOD_Q1 : return("Q1");      // 1 quarter (custom timeframe)
+      case PERIOD_Q1 : return("Q1");      // 1 quarter  (custom timeframe)
    }
    return(StrFormat("%d", period));
 }
@@ -1376,7 +1384,6 @@ const char* WINAPI PeriodDescriptionA(int period) {
 const wchar* WINAPI PeriodDescriptionW(int period) {
    const char* s = PeriodDescriptionA(period);
    if (!s) return(NULL);
-
    return(wcsdup(ansiToUnicode(string(s)).c_str()));
 }
 
@@ -1394,19 +1401,27 @@ const char* WINAPI PeriodToStr(int period) {
    switch (period) {
       case NULL      : return("NULL"      );
       case PERIOD_M1 : return("PERIOD_M1" );     // 1 minute
+      case PERIOD_M2 : return("PERIOD_M2" );     // 2 minutes  (custom timeframe)
+      case PERIOD_M3 : return("PERIOD_M3" );     // 3 minutes  (custom timeframe)
+      case PERIOD_M4 : return("PERIOD_M4" );     // 4 minutes  (custom timeframe)
       case PERIOD_M5 : return("PERIOD_M5" );     // 5 minutes
+      case PERIOD_M6 : return("PERIOD_M6" );     // 6 minutes  (custom timeframe)
+      case PERIOD_M10: return("PERIOD_M10");     // 10 minutes (custom timeframe)
+      case PERIOD_M12: return("PERIOD_M12");     // 12 minutes (custom timeframe)
       case PERIOD_M15: return("PERIOD_M15");     // 15 minutes
+      case PERIOD_M20: return("PERIOD_M20");     // 20 minutes (custom timeframe)
       case PERIOD_M30: return("PERIOD_M30");     // 30 minutes
       case PERIOD_H1 : return("PERIOD_H1" );     // 1 hour
-      case PERIOD_H2 : return("PERIOD_H2" );     // 2 hours (custom timeframe)
-      case PERIOD_H3 : return("PERIOD_H3" );     // 3 hours (custom timeframe)
+      case PERIOD_H2 : return("PERIOD_H2" );     // 2 hours    (custom timeframe)
+      case PERIOD_H3 : return("PERIOD_H3" );     // 3 hours    (custom timeframe)
       case PERIOD_H4 : return("PERIOD_H4" );     // 4 hours
-      case PERIOD_H6 : return("PERIOD_H6" );     // 6 hours (custom timeframe)
-      case PERIOD_H8 : return("PERIOD_H8" );     // 8 hours (custom timeframe)
+      case PERIOD_H6 : return("PERIOD_H6" );     // 6 hours    (custom timeframe)
+      case PERIOD_H8 : return("PERIOD_H8" );     // 8 hours    (custom timeframe)
+      case PERIOD_H12: return("PERIOD_H12");     // 12 hours   (custom timeframe)
       case PERIOD_D1 : return("PERIOD_D1" );     // 1 day
       case PERIOD_W1 : return("PERIOD_W1" );     // 1 week
       case PERIOD_MN1: return("PERIOD_MN1");     // 1 month
-      case PERIOD_Q1 : return("PERIOD_Q1" );     // 1 quarter (custom timeframe)
+      case PERIOD_Q1 : return("PERIOD_Q1" );     // 1 quarter  (custom timeframe)
    }
    return((char*)error(ERR_INVALID_PARAMETER, "unknown parameter period: %d", period));
    #pragma EXPANDER_EXPORT
