6135b2dfcc49590fa7f740f1df323b89b564bcde
Peter Walther <pewa-github@rosasurfer.com>
2025-08-10 14:21:41 +0300

    documentation

--- header/struct/mt4/HistoryBar401.h
+++ header/struct/mt4/HistoryBar401.h
@@ -6,23 +6,23 @@
 /**
  * MT4 struct HISTORY_BAR_401
  *
- * Default bar format in history files since builds > 509, and internal bar format of ArrayCopyRates().
+ * Bar format in history files of terminal builds > 509, and internal bar format of ArrayCopyRates().
  *
  * @see  https://docs.mql4.com/mql4changes
  */
-struct HISTORY_BAR_401 {                           // -- offset --- size --- description --------------------
-   time32 time;                                    //         0        4     MetaQuotes: low part of time64
-   DWORD  _reserved1;                              //         4        4     MetaQuotes: high part of time64
-   double open;                                    //         8        8
-   double high;                                    //        16        8
-   double low;                                     //        24        8
-   double close;                                   //        32        8
-   uint   ticks;                                   //        40        4     MetaQuotes: low part of int64
-   DWORD  _reserved2;                              //        44        4     MetaQuotes: high part of int64
-   int    spread;                                  //        48        4     (unused)
-   uint64 volume;                                  //        52        8     (unused)
-};                                                 // -------------------------------------------------------
-#pragma pack(pop)                                  //               = 60
+struct HISTORY_BAR_401 {                        // -- offset --- size --- description -----------
+   time32 time;                                 //         0        4     low int32 of int64
+   DWORD  _reserved1;                           //         4        4     high int32 of int64
+   double open;                                 //         8        8
+   double high;                                 //        16        8
+   double low;                                  //        24        8
+   double close;                                //        32        8
+   uint   ticks;                                //        40        4     low int32 of int64
+   DWORD  _reserved2;                           //        44        4     high int32 of int64
+   int    spread;                               //        48        4     (unused)
+   uint64 volume;                               //        52        8     (unused)
+};                                              // ----------------------------------------------
+#pragma pack(pop)                               //               = 60
 
 typedef HISTORY_BAR_401 HistoryBar401;
-typedef HISTORY_BAR_401 MqlRates;                  // MetaQuotes alias
+typedef HISTORY_BAR_401 MqlRates;               // MetaQuotes alias
