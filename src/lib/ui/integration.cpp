051d87ff727efbca7d327fed6213a933b01efd93
Peter Walther <pewa-github@rosasurfer.com>
2026-06-15 00:21:12 +0300

    refactor UI integration of the Expander

--- src/lib/ui/integration.cpp
+++ src/lib/ui/integration.cpp
@@ -7,105 +7,76 @@
 #include <commctrl.h>
 
 
-static HHOOK g_hUiHook = NULL;                        // hook handle to subclass the terminal main window from a non-UI thread
-static const uint g_subclassId = 1;                   // main window subclass identifier
+const uint MAIN_WINDOW_SUBCLASS_ID  = 1;              // subclass identifier for the main window
+const uint CHART_WINDOW_SUBCLASS_ID = 2;              // subclass identifier for chart windows
+
+static HHOOK g_hUiHook = NULL;                        // hook handle to switch to the UI thread
 
 
 /**
  * Setup UI integration of the Expander.
+ *
+ * @return BOOL - success status
  */
-void WINAPI SetupUiIntegration() {
-   CustomizeTerminal();
+BOOL WINAPI SetupUiIntegration() {
+   // make sure we run in the UI thread
+   if (!IsUIThread()) {
+      if (g_hUiHook) return FALSE;
+      g_hUiHook = SetWindowsHookEx(WH_CALLWNDPROC, UiThreadProc, NULL, GetUIThreadId());
+      if (!g_hUiHook) return !error(ERR_WIN32_ERROR + GetLastError(), "SetWindowsHookEx()");
+
+      HWND hWndMain = GetTerminalMainWindow();
+      if (!hWndMain) return FALSE;
+
+      // wake-up the UI thread with a non-blocking SendMessage()
+      SetLastError(ERROR_SUCCESS);
+      if (!SendMessageTimeout(hWndMain, WM_NULL, 0, 0, SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG, 3000, NULL)) {
+         debug(ERR_WIN32_ERROR + GetLastError(), "SendMessageTimeout()");
+      }
+      return TRUE;
+   }
+
+   // we run in the UI thread
+   if (!SubclassMainWindow())   return FALSE;      // subclass the terminal main window
+   if (!SubclassChartWindows()) return FALSE;      // subclass all current and future chart windows
+   if (!CustomizeTerminal())    return FALSE;      // customize the terminal as configured
+   return TRUE;
 }
 
 
 /**
- * Customize the UI of the terminal.
+ * Callback function registered by SetWindowsHookEx(). Runs in the UI thread.
+ *
+ * @param  int    code   - whether the hook procedure must process the message
+ * @param  WPARAM wParam - whether the message was sent by the current thread
+ * @param  LPARAM lParam - pointer to message details
+ *
+ * @return LRESULT - return value of CallNextHookEx()
  */
-void WINAPI CustomizeTerminal() {
-   HWND hWndMain = GetTerminalMainWindow();
-   if (!hWndMain) return;
-   BOOL isUiThread = IsUIThread();
-
-   SubclassMainWindow(hWndMain, isUiThread);          // subclass the terminal main window
-   SubclassChartWindows(hWndMain, isUiThread);        // subclass all current and future chart windows
-
-   // get the toolbar
-   HWND hToolbar = GetDlgItem(hWndMain, IDC_TOOLBAR);
-   if (!hToolbar) {
-      error(ERR_WIN32_ERROR + GetLastError(), "GetDlgItem(MainWindow, IDC_TOOLBAR) terminal toolbar not found");
-      return;
-   }
-
-   // a local function as substitute for missing lambda support in C++03
-   struct local {
-      static void CloseWindow(HWND hCtrl, BOOL isUiThread) {
-         if (isUiThread) {
-            DestroyWindow(hCtrl);                     // must be called from the UI thread
-         }
-         else {
-            PostMessageA(hCtrl, WM_CLOSE, 0, 0);
-            while (IsWindow(hCtrl)) Sleep(100);       // TODO: so ugly, move to the UI thread
+static LRESULT CALLBACK UiThreadProc(int code, WPARAM wParam, LPARAM lParam) {
+   if (code >= 0) {
+      if (g_hUiHook) {
+         HHOOK hHook = g_hUiHook;
+         g_hUiHook = NULL;
+         if (!UnhookWindowsHookEx(hHook)) {
+            error(ERR_WIN32_ERROR + GetLastError(), "UnhookWindowsHookEx(hHook=0x%p)", hHook);
          }
       }
-   };
-
-   // find and remove a search box control (contains the "Community" button)
-   if (HWND hSearchCtrl = GetDlgItem(hToolbar, IDC_TOOLBAR_SEARCHBOX)) {
-      local::CloseWindow(hSearchCtrl, isUiThread);
-      if (!RedrawWindow(hToolbar, NULL, NULL, RDW_ERASE|RDW_INVALIDATE)) {
-         error(ERR_WIN32_ERROR + GetLastError(), "RedrawWindow(IDC_TOOLBAR)");
-         return;
-      }
+      SetupUiIntegration();
    }
-   // if search box control not found, find/remove an independent "Community" button
-   else if (HWND hBtnCtrl = GetDlgItem(hToolbar, IDC_TOOLBAR_COMMUNITY_BUTTON)) {
-      local::CloseWindow(hBtnCtrl, isUiThread);
-   }
+   return CallNextHookEx(NULL, code, wParam, lParam);
 }
 
 
 /**
  * Subclass the terminal main window.
  *
- * @param  HWND hWndMain   - handle of the terminal main window
- * @param  BOOL isUiThread - whether the function is executed by the UI thread
- *
  * @return BOOL - success status
  */
-BOOL WINAPI SubclassMainWindow(HWND hWndMain, BOOL isUiThread) {
-   DWORD_PTR data = 0;        // guard against multiple calls
-   if (GetWindowSubclass(hWndMain, MainWindowSubclassProc, g_subclassId, &data)) {
-      return TRUE;
+static BOOL WINAPI SubclassMainWindow() {
+   if (!SetWindowSubclass(GetTerminalMainWindow(), MainWindowSubclassProc, MAIN_WINDOW_SUBCLASS_ID, 0)) {
+      return !error(ERR_WIN32_ERROR + GetLastError(), "SetWindowSubclass()");
    }
-   if (isUiThread) {          // on the UI thread we can call SetWindowSubclass() directly
-      if (!SetWindowSubclass(hWndMain, MainWindowSubclassProc, g_subclassId, 0)) {
-         return !error(ERR_WIN32_ERROR + GetLastError(), "SetWindowSubclass()");
-      }
-   }
-   else {                     // otherwise we must install a hook to be executed by the UI thread
-      g_hUiHook = SetWindowsHookEx(WH_CALLWNDPROC, UiHookProc, NULL, GetUIThreadId());
-      if (!g_hUiHook) return !error(ERR_WIN32_ERROR + GetLastError(), "SetWindowsHookEx()");
-
-      // wake-up the UI thread with a non-blocking SendMessage()
-      SetLastError(ERROR_SUCCESS);
-      if (!SendMessageTimeout(hWndMain, WM_NULL, 0, 0, SMTO_ABORTIFHUNG|SMTO_NOTIMEOUTIFNOTHUNG, 3000, NULL)) {
-         debug(ERR_WIN32_ERROR + GetLastError(), "SendMessageTimeout()");
-      }
-   }
-   return TRUE;
-}
-
-
-/**
- * Subclass all current and future chart windows.
- *
- * @param  HWND hWndMain   - handle of the terminal main window
- * @param  BOOL isUiThread - whether the function is executed by the UI thread
- *
- * @return BOOL - success status
- */
-BOOL WINAPI SubclassChartWindows(HWND hWndMain, BOOL isUiThread) {
    return TRUE;
 }
 
@@ -122,7 +93,7 @@ BOOL WINAPI SubclassChartWindows(HWND hWndMain, BOOL isUiThread) {
  *
  * @return LRESULT - depends on the message sent
  */
-LRESULT CALLBACK MainWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam, UINT_PTR subclassId, DWORD_PTR data) {
+static LRESULT CALLBACK MainWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam, UINT_PTR subclassId, DWORD_PTR data) {
    switch (msg) {
       case WM_COMMAND: {
          static DWORD debugOptions = GetDebugOptions();
@@ -165,6 +136,16 @@ LRESULT CALLBACK MainWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPAR
 
 
 /**
+ * Subclass all current and future chart windows.
+ *
+ * @return BOOL - success status
+ */
+static BOOL WINAPI SubclassChartWindows() {
+   return TRUE;
+}
+
+
+/**
  * A chart window's subclassing window procedure. Executed in the UI thread.
  *
  * @param  HWND      hWnd       - subclassed window receiving the message
@@ -176,32 +157,39 @@ LRESULT CALLBACK MainWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPAR
  *
  * @return LRESULT - depends on the message sent
  */
-LRESULT CALLBACK ChartWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam, UINT_PTR subclassId, DWORD_PTR data) {
+static LRESULT CALLBACK ChartWindowSubclassProc(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam, UINT_PTR subclassId, DWORD_PTR data) {
    return DefSubclassProc(hWnd, msg, wParam, lParam);
 }
 
 
 /**
- * Callback function registered by SetWindowsHookEx() to run in the UI thread.
+ * Customize the UI of the terminal as configured.
  *
- * @param  int    code   - whether the hook procedure must process the message
- * @param  WPARAM wParam - whether the message was sent by the current thread
- * @param  LPARAM lParam - pointer to message details
- *
- * @return LRESULT - return value of CallNextHookEx()
+ * @return BOOL - success status
  */
-LRESULT CALLBACK UiHookProc(int code, WPARAM wParam, LPARAM lParam) {
-   if (code >= 0) {
-      if (g_hUiHook) {
-         HHOOK hHook = g_hUiHook;
-         g_hUiHook = NULL;
-         if (!UnhookWindowsHookEx(hHook)) {
-            error(ERR_WIN32_ERROR + GetLastError(), "UnhookWindowsHookEx(hHook=0x%p)", hHook);
-         }
+static BOOL WINAPI CustomizeTerminal() {
+   HWND hWndMain = GetTerminalMainWindow();
+   if (!hWndMain) return FALSE;
+
+   // get the toolbar
+   HWND hToolbar = GetDlgItem(hWndMain, IDC_TOOLBAR);
+   if (!hToolbar) return !error(ERR_WIN32_ERROR + GetLastError(), "GetDlgItem(MainWindow, IDC_TOOLBAR) not found");
+
+   // find and remove a search box control (contains the "Community" button)
+   if (HWND hSearchCtrl = GetDlgItem(hToolbar, IDC_TOOLBAR_SEARCHBOX)) {
+      if (!DestroyWindow(hSearchCtrl)) {
+         return !error(ERR_WIN32_ERROR + GetLastError(), "DestroyWindow(IDC_TOOLBAR_SEARCHBOX)");;
       }
-      if (!SetWindowSubclass(GetTerminalMainWindow(), MainWindowSubclassProc, g_subclassId, 0)) {
-         error(ERR_WIN32_ERROR + GetLastError(), "SetWindowSubclass()");
+      if (!RedrawWindow(hToolbar, NULL, NULL, RDW_ERASE|RDW_INVALIDATE)) {
+         return !error(ERR_WIN32_ERROR + GetLastError(), "RedrawWindow(IDC_TOOLBAR)");;
       }
+      return TRUE;
    }
-   return CallNextHookEx(NULL, code, wParam, lParam);
+
+   // if search box control not found, find/remove an independent "Community" button
+   HWND hBtnCtrl = GetDlgItem(hToolbar, IDC_TOOLBAR_COMMUNITY_BUTTON);
+   if (hBtnCtrl && !DestroyWindow(hBtnCtrl)) {
+      return !error(ERR_WIN32_ERROR + GetLastError(), "DestroyWindow(IDC_TOOLBAR_COMMUNITY_BUTTON)");;
+   }
+   return TRUE;
 }
