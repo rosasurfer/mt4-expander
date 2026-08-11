49d8178f881cfbc0567798074a83585db149ef3e
Peter Walther <pewa-github@rosasurfer.com>
2026-08-11 11:23:25 +0300

    update Rules Monitor

--- src/mql4/indicators/RulesMonitor.cpp
+++ src/mql4/indicators/RulesMonitor.cpp
@@ -3,18 +3,60 @@
 #include "struct/ExecutionContext.h"
 
 
+#define MQL_PROGRAM_NAME "Rules Monitor"
+
+
 /**
- * Create the indicator's main panel.
+ * Create the status panel.
  *
  * @param  uint pid - indicator pid
  *
  * @return HWND - window handle
  */
-HWND WINAPI RulesMonitor_CreatePanel(uint pid) {
+HWND WINAPI RulesMonitor_CreateStatusPanel(uint pid) {
    // get the EXECUTION_CONTEXT
    if ((int)pid <= 0) return (HWND)!error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
-   EXECUTION_CONTEXT* ec = GetMasterContext(pid); if (!ec) return NULL;
-   if (!StrCompare(ec->programName, "Rules Monitor")) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+   EXECUTION_CONTEXT* ec = GetMasterContext(pid);
+   if (!ec) return NULL;
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+
+   return NULL;
+   #pragma EXPANDER_EXPORT
+}
+
+
+/**
+ * Remove the status panel.
+ *
+ * @param  uint pid - indicator pid
+ *
+ * @return HWND - window handle
+ */
+HWND WINAPI RulesMonitor_RemoveStatusPanel(uint pid) {
+   // get the EXECUTION_CONTEXT
+   if ((int)pid <= 0) return (HWND)!error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
+   EXECUTION_CONTEXT* ec = GetMasterContext(pid);
+   if (!ec) return NULL;
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+
+   return NULL;
+   #pragma EXPANDER_EXPORT
+}
+
+
+/**
+ * Update the status panel.
+ *
+ * @param  uint pid - indicator pid
+ *
+ * @return HWND - window handle
+ */
+HWND WINAPI RulesMonitor_UpdateStatusPanel(uint pid) {
+   // get the EXECUTION_CONTEXT
+   if ((int)pid <= 0) return (HWND)!error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
+   EXECUTION_CONTEXT* ec = GetMasterContext(pid);
+   if (!ec) return NULL;
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
 
    return NULL;
    #pragma EXPANDER_EXPORT
