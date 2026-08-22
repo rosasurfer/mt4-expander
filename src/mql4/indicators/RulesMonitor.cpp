6a4dc303c11c3a790f39930ca73ca7b20f2a9cc9
Peter Walther <pewa-github@rosasurfer.com>
2026-08-22 12:16:50 +0300

    update Rules Monitor

--- src/mql4/indicators/RulesMonitor.cpp
+++ src/mql4/indicators/RulesMonitor.cpp
@@ -9,17 +9,18 @@
 extern "C" IMAGE_DOS_HEADER          __ImageBase;     // this DLL's module handle
 #define HMODULE_EXPANDER ((HMODULE) &__ImageBase)
 
-#define MQL_PROGRAM_NAMEA  "Rules Monitor"
-#define MQL_PROGRAM_NAMEW L"Rules Monitor"
-
+#define MQL_PROGRAM_NAME      "Rules Monitor"
+#define USERDATA_HWND_PANEL   0
 
 // container for view data of the status panel
 struct VIEW_DATA {
+   int trend;
+
+   // --- old ------
    double profit;
    uint   trades;
    wchar  text[256];
 };
-
 
 
 /**
@@ -34,7 +35,13 @@ HWND WINAPI RulesMonitor_CreateStatusPanel(uint pid) {
    if ((int)pid <= 0) return (HWND)!error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
    EXECUTION_CONTEXT* ec = GetMasterContext(pid);
    if (!ec) return NULL;
-   if (!StrCompare(ec->programName, MQL_PROGRAM_NAMEA)) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return (HWND)!error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+
+   // return an existing panel
+   HWND hWndPanel = (HWND)ec->userData[USERDATA_HWND_PANEL];
+   if (hWndPanel && IsWindow(hWndPanel)) {
+      return hWndPanel;
+   }
 
    // register the window class
    WNDCLASSW wc = {};
@@ -51,20 +58,23 @@ HWND WINAPI RulesMonitor_CreateStatusPanel(uint pid) {
    struct local {
       static LRESULT CALLBACK CreatePanel(LPARAM lParam) {
          ARGS* args = (ARGS*)lParam;
-         if (!args) return !error(ERR_INVALID_PARAMETER, "invalid arguments: (null)");
+         if (!args) return !error(ERR_INVALID_PARAMETER, "invalid arguments: NULL");
 
-         DWORD styles = WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS;
-         styles |= WS_CAPTION | WS_SYSMENU;
+         DWORD stdStyles = WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS;
+               stdStyles |= WS_CAPTION | WS_SYSMENU;
+         DWORD extStyles = WS_EX_DLGMODALFRAME;
+         wstring windowText = ansiToUtf16(string(MQL_PROGRAM_NAME));
+
          HWND hWnd = CreateWindowExW(
-            0,                                        // extended styles
-            args->className,                          // class name
-            MQL_PROGRAM_NAMEW,                        // window text
-            styles,                                   // regular styles
-            300, 200, 300, 150,                       // position/size
-            args->hWndParent,                         // parent window
-            0,                                        // control id
-            HMODULE_EXPANDER,                         // module instance
-            args->viewData                            // additional user data
+            extStyles,                             // extended styles
+            args->className,                       // class name
+            windowText.c_str(),                    // window text
+            stdStyles,                             // regular styles
+            300, 200, 300, 150,                    // position + size
+            args->hWndParent,                      // parent window
+            0,                                     // control id
+            HMODULE_EXPANDER,                      // module instance
+            args->viewData                         // user data
          );
          if (!hWnd) error(args->error = ERR_WIN32_ERROR + GetLastError(), "CreateWindowExW()");
          return (LRESULT)hWnd;
@@ -73,17 +83,25 @@ HWND WINAPI RulesMonitor_CreateStatusPanel(uint pid) {
    struct ARGS {
       __in  HWND         hWndParent;
       __in  const wchar* className;
-      __in  void*        viewData;
+      __in  VIEW_DATA*   viewData;
       __out DWORD        error;
-   } args = { ec->chart, wc.lpszClassName, malloc(sizeof(VIEW_DATA)), NO_ERROR };
+   } args = {
+      ec->chart,
+      wc.lpszClassName,
+      (VIEW_DATA*)calloc(sizeof(VIEW_DATA), 1),
+      NO_ERROR,
+   };
 
    // create the panel
    SetLastError(NO_ERROR);
-   HWND hWndPanel = (HWND) InvokeUiThread(local::CreatePanel, (LPARAM)&args);
+   hWndPanel = (HWND) InvokeUiThread(local::CreatePanel, (LPARAM)&args);
    if (!hWndPanel || args.error) return (HWND)!error(orElse(args.error, GetLastError()), "CreatePanel()");
 
-   // move to top of Z-order
+   // move it to the top
    SetWindowPos(hWndPanel, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE|SWP_NOSIZE);
+
+   // update the EXECUTION_CONTEXT
+   ec_SetUserData(ec, USERDATA_HWND_PANEL, (DWORD)hWndPanel);
    return hWndPanel;
    #pragma EXPANDER_EXPORT
 }
@@ -101,8 +119,17 @@ BOOL WINAPI RulesMonitor_DestroyStatusPanel(uint pid) {
    if ((int)pid <= 0) return !error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
    EXECUTION_CONTEXT* ec = GetMasterContext(pid);
    if (!ec) return FALSE;
-   if (!StrCompare(ec->programName, MQL_PROGRAM_NAMEA)) return !error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return !error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
 
+   // destroy the panel
+   HWND hWndPanel = (HWND)ec->userData[USERDATA_HWND_PANEL];
+   if (hWndPanel && IsWindow(hWndPanel)) {
+      if (IsUiThread()) DestroyWindow(hWndPanel);
+      else              PostMessageA(hWndPanel, WM_CLOSE, 0, 0);
+   }
+
+   // update the EXECUTION_CONTEXT
+   ec_SetUserData(ec, USERDATA_HWND_PANEL, NULL);
    return TRUE;
    #pragma EXPANDER_EXPORT
 }
@@ -111,16 +138,23 @@ BOOL WINAPI RulesMonitor_DestroyStatusPanel(uint pid) {
 /**
  * Update the status panel.
  *
- * @param  uint pid - indicator pid
+ * @param  uint pid   - indicator pid
+ * @param  int  trend - trend direction
  *
  * @return bool - success status
  */
-BOOL WINAPI RulesMonitor_UpdateStatusPanel(uint pid) {
+BOOL WINAPI RulesMonitor_UpdateStatusPanel(uint pid, int trend) {
    // get the EXECUTION_CONTEXT
    if ((int)pid <= 0) return !error(ERR_INVALID_PARAMETER, "invalid parameter pid: %d (not a program id)", (int)pid);
    EXECUTION_CONTEXT* ec = GetMasterContext(pid);
    if (!ec) return FALSE;
-   if (!StrCompare(ec->programName, MQL_PROGRAM_NAMEA)) return !error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+   if (!StrCompare(ec->programName, MQL_PROGRAM_NAME)) return !error(ERR_ILLEGAL_STATE, "illegal caller: \"%s\"", ec->programName);
+
+   // get the panel
+   HWND hWndPanel = (HWND)ec->userData[USERDATA_HWND_PANEL];
+   if (!hWndPanel) return !error(ERR_ILLEGAL_STATE, "status panel not found");
+
+   debug("tick=%d  trend=%d", ec->ticks, trend);
 
    return TRUE;
    #pragma EXPANDER_EXPORT
@@ -157,7 +191,7 @@ LRESULT CALLBACK StatusPanelWindowProc(HWND hWnd, uint msg, WPARAM wParam, LPARA
       // make the whole client area draggable
       case WM_NCHITTEST: {
          LRESULT hit = DefWindowProc(hWnd, msg, wParam, lParam);
-         return (hit == HTCLIENT) ? HTCAPTION : hit;  // mouse handling must handle NC message variants
+         return (hit == HTCLIENT) ? HTCAPTION : hit;  // mouse handling must process NC message variants
       }
 
       case WM_COMMAND: {
